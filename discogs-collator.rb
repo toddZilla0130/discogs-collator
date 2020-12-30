7e5cbd4a84e1c4477a14005899dee46b5e5edf0d
Todd Steinwart <toddsteinwart@Todds-MacBook-Pro.local>
2020-12-30 10:10:53 -0500

    just some notes rn.

---
+++ discogs-collator.rb
@@ -0,0 +1,16 @@
+#! /usr/bin/ruby
+
+=begin
+1.  validate command line args:
+    if 2 args, source == arg[0]; target == arg[1]
+    if 3rd arg, output == arg[2]
+    if not 3rd arg, output== arg[2]-collated.csv (figure it out)
+    source && target must exist, or fail
+    for now, blithely overwrite ouput if it exists. may prompt in a future iteration
+2.  flow:
+    open both files
+    read source into a class that makes it easy to query "artist" and "collating artist" fields.
+    for each line of target:
+
+3.  classes:
+=end
