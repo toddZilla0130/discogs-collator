f54ee188d6498db65c166e5b67e3c15fef49bcbe
Todd Steinwart <toddsnc@gmail.com>
2020-12-30 10:22:43 -0500

    picky shit

--- discogs-collator.rb
+++ discogs-collator.rb
@@ -4,9 +4,9 @@ require 'csv'
 
 =begin
 1.  validate command line args:
-    if 2 args, source == arg[0]; target == arg[1]
-    if 3rd arg, output == arg[2]
-    if not 3rd arg, output== arg[2]-collated.csv (figure it out)
+    if 2 args, source = arg[0]; target = arg[1]
+    if 3rd arg, output = arg[2]
+    if not 3rd arg, output = arg[2]-collated.csv (figure it out)
     source & target must exist, or print usage & bail.
     for now, blithely overwrite ouput if it exists. may prompt in a future iteration
 2.  flow:
