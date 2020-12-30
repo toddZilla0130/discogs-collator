215958348b480741b3353ec79a391853420417ca
Todd Steinwart <toddsteinwart@Todds-MacBook-Pro.local>
2020-12-30 10:17:13 -0500

    a thing or two

--- discogs-collator.rb
+++ discogs-collator.rb
@@ -1,11 +1,13 @@
 #! /usr/bin/ruby
 
+require 'csv'
+
 =begin
 1.  validate command line args:
     if 2 args, source == arg[0]; target == arg[1]
     if 3rd arg, output == arg[2]
     if not 3rd arg, output== arg[2]-collated.csv (figure it out)
-    source && target must exist, or fail
+    source & target must exist, or print usage & bail.
     for now, blithely overwrite ouput if it exists. may prompt in a future iteration
 2.  flow:
     open both files
